7fa62626e084a8677a274fd38eedcd1df1fed516
Ben Asselmeier <benasselmeier@mac.lan>
2024-11-19 23:42:25 -0600

    more changes

--- start-main.sh
+++ start-main.sh
@@ -1,6 +1,8 @@
 # comment
 #!/bin/bash
+# Change to the application directory
+cd /home/pi/Desktop/get-recd
 # Activate the virtual environment
 source /home/pi/myenv/bin/activate
 # Run the Python script
-python /home/pi/Desktop/get-recd/main.py
+python /home/pi/Desktop/your-application-directory/main.py
