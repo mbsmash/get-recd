c5258361161590d53020f3cc4cafe4c20930f8a5
Ben Asselmeier <benasselmeier@bens-mbp.lan>
2024-11-20 18:16:54 -0600

    new venv configs

--- start-main.sh
+++ start-main.sh
@@ -1,8 +1,20 @@
-# comment
 #!/bin/bash
-# Change to the application directory
-cd /home/pi/Desktop/get-recd
+
+# Define the virtual environment directory
+VENV_DIR="venv"
+
+# Check if the virtual environment directory exists
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Virtual environment not found. Creating one..."
+    python3 -m venv $VENV_DIR
+    echo "Virtual environment created."
+fi
+
 # Activate the virtual environment
-source /home/pi/myenv/bin/activate
-# Run the Python script
-python /home/pi/Desktop/your-application-directory/main.py
+source $VENV_DIR/bin/activate
+
+# Install the required dependencies
+pip install -r requirements.txt
+
+# Run the main script
+python main.py
